2bf89e83d88430809dd83ac01ff5b19c23306f2e
eros.montin <eros.montin@gmail.com>
2023-11-27 15:09:17 -0500

    v0.9

--- mroptimum/t.py
+++ mroptimum/t.py
@@ -1,79 +1,20 @@
-# # import multiprocessing
-
-# # def job(num):
-# #     return num * 2
-# # p = multiprocessing.Pool(processes=5)
-# # data = p.map(job, [i for i in range(20)])
-# # p.close()
-# # print(data)
-# import os
-# import boto3
-
-# import shutil
-
-# mroptimum_result="mroptimum-result"
-
-# def s3FileTolocal(J,s3=None,pt="/tmp"):
-#     key=J["key"]
-#     bucket=J["bucket"]
-#     filename=J["filename"]
-#     if s3==None:
-#         ID=os.getenv("AWS_ACCESS_KEY_ID")
-#         SA_ID=os.getenv("AWS_SECRET_ACCESS_KEY")
-#         TK=os.getenv("AWS_SESSION_TOKEN")
-#         s3 = boto3.resource('s3',aws_access_key_id=ID,
-#     aws_secret_access_key=SA_ID,aws_session_token=TK
-#     )
-#     O=pn.Pathable(pt)
-#     O.addBaseName(filename)
-#     O.changeFileNameRandom()
-#     f=O.getPosition()
-#     s3.Bucket(bucket).download_file(key,f)
-#     J["filename"]=f
-#     J["type"]="local"
-#     return J
-
-# from pynico_eros_montin import pynico as pn
-# jf="/g/mroptimumCU/pmrgrappa.json"
-# J=pn.Pathable(jf).readJson()
-
-# T=J["task"]
-
-# print(T["options"]["reconstructor"]["options"]["noise"]["options"])
-
-# if (T["options"]["reconstructor"]["options"]["noise"]["options"]["type"])=="s3":
-#     T["options"]["reconstructor"]["options"]["noise"]["options"]=s3FileTolocal(T["options"]["reconstructor"]["options"]["noise"]["options"])
-# if (T["options"]["reconstructor"]["options"]["signal"]["options"]["type"])=="s3":
-#     T["options"]["reconstructor"]["options"]["signal"]["options"]=s3FileTolocal(T["options"]["reconstructor"]["options"]["signal"]["options"])
-
-# JO=pn.createRandomTemporaryPathableFromFileName("a.json")
-# JO.writeJson(T)
-# #create a directory for the calculation to be zippedin the end
-# O=pn.createRandomTemporaryPathableFromFileName("a.json")
-# O.appendPath('OUT')
-# O.ensureDirectoryExistence()
-# OUT=O.getPosition()
-# #run mr optimum
-# K=pn.BashIt()
-# K.setCommand(f"MRO/bin/python -m mroptimum.snr -j {JO.getPosition()} -o {OUT}")
-# K.run()
-# Z=pn.createRandomTemporaryPathableFromFileName('a.zip')
-# print("Zipping the file")
-# shutil.make_archive(Z.getPosition()[:-4], 'zip', O.getPath())
-# print("start uploading")
-# ID=os.getenv("AWS_ACCESS_KEY_ID")
-# SA_ID=os.getenv("AWS_SECRET_ACCESS_KEY")
-# TK=os.getenv("AWS_SESSION_TOKEN")
-# s3 = boto3.resource('s3',aws_access_key_id=ID,aws_secret_access_key=SA_ID,aws_session_token=TK)
-# s3.Bucket("mroptimum-result").upload_file(Z.getPosition(),Z.getBaseName())
-import twixtools as tx
-import pygrappa
+import cloudmrhub.cm2D as cm2D
+from pynico_eros_montin import pynico as pn
 import numpy as np
+import matplotlib.pyplot as plt
 
+import twixtools as tx
+import numpy as np 
 
-# n='/data/MYDATA/siemensrawdataexamples/Sebastian/raw/meas_MID02696_FID183824_gre_GRAPPA_2.dat'
-n='/data/MYDATA/siemensrawdataexamples/Sebastian/raw/meas_MID02697_FID183825_gre_GRAPPA_3.dat'
-twix=tx.map_twix(n)
+import matplotlib.pyplot as plt
+
+import cloudmrhub.cm as cm
+
+from  mro import *
+
+# s='/data/MYDATA/siemensrawdataexamples/Sebastian/raw/meas_MID02696_FID183824_gre_GRAPPA_2.dat'
+s='/data/MYDATA/siemensrawdataexamples/Sebastian/raw/meas_MID02699_FID183827_gre_mSENSE_2.dat'
+twix=tx.map_twix(s)
 H=twix[1]["hdr"]
 iPat=H['MeasYaps']['sPat']
 
@@ -86,7 +27,7 @@ im_array.flags['remove_os'] = True  # activate automatic os removal
 im_array.flags['average']['Rep'] = True  # average all repetitions
 im_array.flags['average']['Ave'] = True # average all repetitions
 
-signal=np.transpose(im_array[0,0,0,0,0,0,0,0,0,0,sl,0,0,:,:,:],[2,0,1])  
+signal=cm.fixAccelratedKSpace2D(np.transpose(im_array[0,0,0,0,0,0,0,0,0,0,sl,0,0,:,:,:],[2,0,1]),2)
 print('---noise----')
 n_array = twix[0]['noise']
 
@@ -99,34 +40,65 @@ noise=np.transpose(n_array[0,0,0,0,0,0,0,0,0,0,sl,0,0,:,:,:],[2,0,1])
 
 print('---refscan----')
 r_array = twix[1]['refscan']
-
 r_array.flags['remove_os'] = True  # activate automatic os removal
 r_array.flags['average']['Rep'] = True  # average all repetitions
-im_array.flags['average']['Ave'] = True # average all repetitions
+r_array.flags['average']['Ave'] = True # average all repetitions
 
-ref=np.transpose(r_array[0,0,0,0,0,0,0,0,0,0,sl,0,0,:,:,:],[2,0,1])  
+ref_=np.transpose(r_array[0,0,0,0,0,0,0,0,0,0,sl,0,0,:,:,:],[2,0,1])  
+n_ref=ref_.shape[1]
 
-print("signal",signal.shape,"noise",noise.shape,"ref",ref.shape)
-# 76 lines instead of 24
-
-from cloudmrhub.cm2D import cm2DReconRSS
-# pygrappa.grappa(,)
+ref=np.zeros_like(signal)
+ref[:,0:n_ref]=ref_
 
 
-import matplotlib.pyplot as plt
-def recon(signal,noise,tt):
-    R=cm2DReconRSS()
-    R.setNoiseKSpace(noise)
-    R.setSignalKSpace(signal)
-    plt.imshow(R.getOutput())
-    plt.title(tt)
-    plt.show()
+O=dict()
+O["signal"]=signal
+O["noise"]=noise
+O["noisecovariance"]=None
+O["reference"]=ref
+O["mimic"]=False
+O["accelleration"]=[1,int(iPat['lAccelFactPE'])]
+O["autocalibration"]=[np.nan,int(iPat['lRefLinesPE'])]
+O["grappakernel"]=None
+O["slice"]=1
 
-recon(signal,noise,"signal")
+# reconstructor=cm2D.cm2DKellmanSENSE()
 
-recon(ref,noise,"ref")
-import pygrappa
 
-K=pygrappa.grappa(signal,ref,kernel_size=(10,10))
-print(K.shape)
-recon(K,noise,"signal,ref")
+# OUT=calcKellmanSNR(reconstructor,O)
+
+
+
+reconstructor=cm2D.cm2DReconSENSE()
+
+# O["NR"]=4
+# OUT=calcPseudoMultipleReplicasSNR(reconstructor,O)
+
+
+O["NR"]=2
+O["boxSize"]=2
+OUT=calcPseudoMultipleReplicasSNRWien(reconstructor,O)
+
+
+print(OUT["SNR"])
+# s="/data/MYDATA/siemensrawdataexamples/15042019_MR/meas_MID00036_FID188190_Multislice_100_REPLICAS.dat"
+
+# twix=twixtools.map_twix(s)
+# im_array = twix[0]['image']
+# im_array.flags['remove_os'] = True  # activate automatic os removal
+# im_array.flags['average']['Rep'] = False  # average all repetitions
+# im_array.flags['average']['Ave'] = True  # average all repetitions
+# SL=0
+# S=np.transpose(im_array[0,0,0,0,0,0,0,:,0,0,0,SL,0,:,:,:],[0,3,1,2])
+# O["signal"]=S
+
+# O["mimic"]=True
+# O["accelleration"]=[1,int(iPat['lAccelFactPE'])]
+# O["autocalibration"]=[np.nan,int(iPat['lRefLinesPE'])]
+# O["noise"]=None
+
+# OUT=calcMultipleReplicasSNR(reconstructor,O)
+
+
+
+print(OUT["SNR"])
